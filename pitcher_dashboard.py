430b56fa86a41e19c43e5242cc80c5eabf1fb4e6
Squirrly17 <Zane.Ohman17@gmail.com>
2025-05-09 03:58:18 -0500

    Update pitcher_dashboard.py

--- pitcher_dashboard.py
+++ pitcher_dashboard.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 
 # Load today’s merged data
 today_str = date.today().strftime("%Y-%m-%d")
-file_path = f"data/dashboard_merged_{today_str}.csv"
+file_path = f"dashboard_merged_{today_str}.csv"
 
 try:
     df = pd.read_csv(file_path)
